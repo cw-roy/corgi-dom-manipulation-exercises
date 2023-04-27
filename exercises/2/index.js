939d3d717a381f822cc55e4b855b52586a699efa
cw-roy <curtis_roy@msn.com>
2023-04-27 07:50:39 -0700

    Exercise 2

---
+++ exercises/2/index.js
@@ -0,0 +1,7 @@
+// Exercise 1:  change the h1 element
+const h1Element = document.querySelector('h1');
+h1Element.textContent = 'CORGIS!';
+
+// Exercise 2:  change the paragraph
+const paragraphElement = document.querySelector('p');
+paragraphElement.textContent = 'Two types of corgi lords: Pembroke and Cardigan'
